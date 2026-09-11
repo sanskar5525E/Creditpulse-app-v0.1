36414df6eccad1fced67cce29c972319517cb8fa
Sanskarjyoti <sanskarjyoti45@gmail.com>
2026-09-11 22:30:30 +0530

    Replace Supabase URL and key with placeholders

--- Frontend/supabase.js
+++ Frontend/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://mkjclbhwjnprrqtejmfa.supabase.co"
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ramNsYmh3am5wcnJxdGVqbWZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1OTUzOTcsImV4cCI6MjA5NDE3MTM5N30.8lF66ixFoS9vdGFH-M-XplDrsNIyEO6X7vIA9cyJynI"
+const SUPABASE_URL = "SUPABASE_URL"
+const SUPABASE_KEY = "SUPABASE_KEY"
 
 export const supabase = createClient(
 SUPABASE_URL,
